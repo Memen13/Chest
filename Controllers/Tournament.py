c3ae1ea86d8676779051bde49d0c2304b8ac0484
Memen13 <aymen.klai@yahoo.fr>
2021-04-07 16:02:38 +0200

    1rst commit

---
+++ Controllers/Tournament.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from Models.Tournament import TournamentModel
+
+class CreateTournament(TournamentModel):
+
+     def new_tournament():
+         tournois = TournamentModel()
+
+     def add_players():
+         pass
+
+    def create_round():
+         pass
+
+    def tinydb():
+         pass
+
---
+++ Models/Data.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from tinydb import TinyDB, Query, where
+
+
+class Data():
+    """
+    Class générale héritée sur les autres models afin de gérer la base de donnée
+
+    """
+
+    db = TinyDB('data/db.json')
+
+    def save(self, data):
+        table = Data.db.table(self.__class__.__name__)
+        Data.db.insert(self.__dict__)
+
+    def update(self):
+        table = Data.db.table(self.__class__.__name__)
+        query = Query()
+        table.update(self.)
+
+    def delete(self):
+        table = Data.db.table(self.__class__.__name__)
+        query = Query()
+        table.remove()
+
+    def search(self):
+        table = Data.db.table(self.__class__.__name__)
+        Data.get()
+
