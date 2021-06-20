b9a20e660316280f36dfbe241ef9f1f6902a0789
Memen13 <aymen.klai@yahoo.fr>
2021-06-20 20:56:17 +0200

    correction des controlleurs

---
+++ Models/Data_players.py
@@ -0,0 +1,25 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from tinydb import TinyDB
+
+def create_data():
+    db = TinyDB('PlayerDatabase.json')
+    table = db.table("Players")
+    global table
+
+def insert(serial):
+     table.insert(serial)
+
+def update():
+    #Prendre l'id du joueur
+    #table.search(where("id") == "METTRE L'ID DU JOUEUR"
+    #Créer une nouvelle fonction pour la modification du joueur
+
+
+# ajouter une condition pour vérifier si la bss existe avant d'instancier
+       """ def update(self):
+            self.table.insert({"name": self.objectTournament.name})
+
+
+
